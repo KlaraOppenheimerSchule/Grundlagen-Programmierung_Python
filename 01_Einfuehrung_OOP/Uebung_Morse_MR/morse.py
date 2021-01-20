b4b6d43424c9d6bace739a83f200fcad9ff3206b
maxrhm <71386751+maxrhm@users.noreply.github.com>
2021-01-20 08:44:11 +0100

    Ein paar Änderungen im Code

--- 01_Einfuehrung_OOP/Uebung_Morse_MR/morse.py
+++ 01_Einfuehrung_OOP/Uebung_Morse_MR/morse.py
@@ -1,9 +1,10 @@
-morse = { "A": ".-", "B": "-...", "C": "-.-.",
+class morsetrans:
+    morse = { "A": ".-", "B": "-...", "C": "-.-.",
           "D": "-..", "E": ".", "F": "..-.",
           "G": "--.", "H": "....", "I": "..",
           "J": ".---", "K": "-.-", "L": ".-..",
           "M": "--", "N": "-.", "O": "---",
-          "P": ".--.", "Q": "--.-", "R": ".--",
+          "P": ".--.", "Q": "--.-", "R": ".-.",
           "S": "...", "T": "-", "U": "..-",
           "V": "...-", "W": ".--", "X": "-..-",
           "Y": "-..-", "Z": "--..", "1": ".----",
@@ -11,13 +12,19 @@ morse = { "A": ".-", "B": "-...", "C": "-.-.",
           "5": ".....", "6": "-....", "7": "--...",
           "8": "---..", "9": "----.", "0": "-----"
 }
+    letter = ""
+    def eingabe(self):
+        self.letter = input("Bitte geben Sie einen Satz ein:")
+        self.letter = self.letter.upper()
 
-letter = input("Bitte geben Sie einen Satz ein:")
-letter = letter.upper()
+    def übersetzung(self):
+        for i in range(0, len(self.letter)):
+            if self.letter[i] == " ":
+                print(" ")
+            for key in self.morse:
+                if key == self.letter[i]:
+                    print(self.morse[key])
 
-for i in range(0, len(letter)):
-    if letter[i] == " ":
-        print(" ")
-    for key in morse:
-        if key == letter[i]:
-            print(morse[key])
+morse = morsetrans()
+morse.eingabe()
+morse.übersetzung()
